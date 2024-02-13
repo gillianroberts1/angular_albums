39525a0c5eab049e41f03679cb74e48d411f2ee4
gillianroberts1 <138917227+gillianroberts1@users.noreply.github.com>
2024-02-13 15:00:24 +0000

    first commit

---
+++ src/app/albums/albums.component.ts
@@ -0,0 +1,36 @@
+import { NgFor } from '@angular/common';
+import { Component } from '@angular/core';
+import { AlbumsServiceService } from '../albums-service.service';
+
+@Component({
+  selector: 'app-albums',
+  standalone: true,
+  imports: [NgFor],
+  template: `
+    <h2>{{ title }}</h2>
+    <ul>
+      <li *ngFor="let album of albums">
+        {{ album }}
+      </li>
+    </ul>
+    <button (click)="onSave($event)">Save</button>
+  `,
+  styleUrl: './albums.component.scss',
+})
+export class AlbumsComponent {
+  title = 'List of Albums';
+  albums: any;
+  isActive = true;
+
+  constructor(service: AlbumsServiceService) {
+    this.albums = service.getAlbums();
+  }
+
+  onSave($event: any){
+    console.log("Button was clicked", $event)
+  }
+}
+
+//
+
+// template: '<h2>{{"Title: " + getTitle()}}</h2>',
